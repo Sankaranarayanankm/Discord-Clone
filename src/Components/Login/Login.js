af22985f912284edb761dfe2cb4d68fb3ce4f934
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2024-09-28 17:41:25 +0530

    added login page and completed css

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Login.css";
+import { Button } from "@mui/material";
 
 const Login = () => {
+  const signIn = (e) => {
+    e.preventDefault();
+    // do clever google login here
+  };
   return (
     <div className="login">
-      <h2>Login</h2>
+      <div className="login__logo">
+        <img
+          src="https://blog.logomyway.com/wp-content/uploads/2020/12/discord-logo-4.jpg"
+          alt="Discord Logo"
+        />
+      </div>
+      <Button onClick={signIn}>Sign In</Button>
     </div>
   );
 };
