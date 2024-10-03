0149f2d8784811b30b91777489fba5c19e7c1528
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2024-10-03 14:16:27 +0530

    completed the build

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Chat.css";
 import ChatHeader from "../ChatHeader/ChatHeader";
 import {
@@ -11,24 +11,81 @@ import Message from "../Message/Message";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../store/slice/userSlice";
 import { selectChannelId, selectChannelName } from "../../store/slice/appSlice";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
+import db from "../../firebase";
 
 const Chat = () => {
   const user = useSelector(selectUser);
   const channelId = useSelector(selectChannelId);
   const channelName = useSelector(selectChannelName);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    if (channelId) {
+      const messagesQuery = query(
+        collection(doc(db, "channels", channelId), "messages"),
+        orderBy("timestamp", "desc")
+      );
+
+      onSnapshot(messagesQuery, (snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
+    }
+  }, [channelId]);
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(doc(db, "channels", channelId), "messages"), {
+        message: input,
+        user,
+        timestamp: serverTimestamp(),
+      });
+      setInput("");
+    } catch (error) {
+      console.log(error.message);
+    }
+    
+  };
   return (
     <div className="chat">
       <ChatHeader channelName={channelName} />
       <div className="chat__messages">
-        <Message />
-        <Message />
-        <Message />
+        {messages.map((item) => (
+          <Message
+            timestamp={item.timestamp}
+            message={item.message}
+            user={item.user}
+          />
+        ))}
       </div>
       <div className="chat__input">
         <AddCircle fontSize="large" />
         <form>
-          <input placeholder="Message #Test channel" type="text" />
-          <button className="chat__inputButton" type="submit">
+          <input
+            value={input}
+            placeholder={
+              channelName != null ? `Message ${channelName}` : "Select Channel"
+            }
+            type="text"
+            disabled={!channelId}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button
+            onClick={sendMessage}
+            className="chat__inputButton"
+            type="submit"
+          >
             Send Message
           </button>
         </form>
--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -2,16 +2,19 @@ import React from "react";
 import "./Message.css";
 import { Avatar } from "@mui/material";
 
-const Message = () => {
+const Message = ({ timestamp, message, user }) => {
+  console.log(user);
   return (
     <div className="message">
-      <Avatar src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+      <Avatar src={user.photo} />
       <div className="message__info">
         <h4>
-          Username
-          <span className="message__timestamp">This is a timestamp</span>
+          {user.displayName}
+          <span className="message__timestamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
         </h4>
-        <p>This is the message!</p>
+        <p>{message}</p>
       </div>
     </div>
   );
